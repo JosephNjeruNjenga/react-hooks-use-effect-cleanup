f65a1d0e60f581f24ae967ed317852be73733391
+ <+>
2022-09-29 23:37:16 +0300

    Completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,11 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
-      setTime(new Date());
-    }, 1000);
+    const timerID = setInterval(() => {
+       setTime(new Date());
+     }, 1000);
+
+    return  () => clearInterval(timerID)
   }, []);
 
   return <div>{time.toString()}</div>;
